de38ff02e28ba6dc8665e9e9355b2326735ef147
Shuang.W <66452432+swnotmetal@users.noreply.github.com>
2024-10-23 02:39:00 +0300

    fullbuild.sh now also launch frontend

--- bloglist-server/fullbuild.sh
+++ bloglist-server/fullbuild.sh
@@ -1,4 +1,15 @@
-echo "starting the full build process"
+#!/bin/bash
 
+echo "Starting the full build process"
+
+# Install backend dependencies
 npm install
+
+# Build the frontend
+cd ./bloglist-client && npm install && npm run build
+
+# Go back to the backend directory
+cd ..
+
+# Start the backend server
 npm run start
