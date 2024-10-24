fc5704d6cfd3e88675b2f82c28dfb1c2aa9cee5e
Shuang.W <66452432+swnotmetal@users.noreply.github.com>
2024-10-24 06:11:57 +0300

    ffs

--- bloglist-server/fullbuild.sh
+++ bloglist-server/fullbuild.sh
@@ -14,8 +14,11 @@ npm install
 echo "Building bloglist-client"
 npm run build
 
+echo "Creating dist directory in bloglist-server if it doesn't exist"
+mkdir -p ../bloglist-server/dist
+
 echo "Copying dist folder to bloglist-server"
-cp -r dist ../bloglist-server/dist
+cp -r dist/* ../bloglist-server/dist
 
 echo "Navigating to bloglist-server"
 cd ../bloglist-server
@@ -24,4 +27,3 @@ echo "Building bloglist-server"
 npm run build
 
 echo "Full build process completed successfully"
-
